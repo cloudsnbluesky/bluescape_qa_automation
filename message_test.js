0962073c6aa1bad1ce7c51a42b6ccd5264625e2f
Ben N <benjaminng387@gmail.com>
2022-05-17 16:57:17 -0700

    using variables

--- message_test.js
+++ message_test.js
@@ -1,38 +1,40 @@
+const url = 'https://bluescapeqainterview.wordpress.com/contact/'
+
 Feature('Testing Send Us a Message page');
 
 // Check if website is accessible
 Scenario('open website', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
 });
 
 // Check if name input field is present
 Scenario('check name input field is present and required', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
     I.seeElement({'xpath':'//*[@id="g7-name" and @required]'})
 });
 
 // Check if email input field is present and required
 Scenario('check email input field is present and required', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
     I.seeElement({'xpath':'//*[@id="g7-email" and @required]'})
 });
 
 // Check if website input field is present
 Scenario('check website input field is present', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
     I.seeElement({'xpath':'//*[@id="g7-website"]'})
 });
 
 // Check date input field is present and required
 Scenario('check date input field is present and required', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
     I.seeElement({'xpath':'//*[@id="g7-date" and @required]'})
 });
 
 // Try to submit a form with an invalid website
 // User should not see "Message Sent" success page
 Scenario('submit invalid website', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
     I.fillField({'xpath':'//*[@id="g7-name"]'}, "TEST")
     I.fillField({'xpath':'//*[@id="g7-email"]'}, "TEST@TEST.COM")
     I.fillField({'xpath':'//*[@id="g7-website"]'}, "test")
@@ -45,7 +47,7 @@ Scenario('submit invalid website', ({ I }) => {
 // Verify happy path with a valid form submitted
 // User should see "Message Sent" success page
 Scenario('Happy path: submit valid form', ({ I }) => {
-    I.amOnPage('https://bluescapeqainterview.wordpress.com/contact/')
+    I.amOnPage(url)
     I.fillField({'xpath':'//*[@id="g7-name"]'}, "TEST")
     I.fillField({'xpath':'//*[@id="g7-email"]'}, "TEST@TEST.COM")
     I.fillField({'xpath':'//*[@id="g7-website"]'}, "http://www.test.com")
